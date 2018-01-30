6698ee7a4f7738274903ff7309553e4aa7cc3c34
Alex Sartan <asartan@pivotal.io>
2018-01-30 16:51:40 -0700

    Database connection changes

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 
 namespace PalTracker
 {
@@ -27,15 +28,17 @@ namespace PalTracker
 
             services.AddSingleton(sp => new WelcomeMessage(
                Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")
-           ));
-           services.AddSingleton(sp => new CloudFoundryInfo(
-               Configuration.GetValue<string>("PORT", "PORT not configured."),
-               Configuration.GetValue<string>("MEMORY_LIMIT", "MEMORY_LIMIT not configured."),
-               Configuration.GetValue<string>("CF_INSTANCE_INDEX", "CF_INSTANCE_INDEX not configured."),
-               Configuration.GetValue<string>("CF_INSTANCE_ADDR", "CF_INSTANCE_ADDR not configured.")
-           ));
+            ));
+            services.AddSingleton(sp => new CloudFoundryInfo(
+                Configuration.GetValue<string>("PORT", "PORT not configured."),
+                Configuration.GetValue<string>("MEMORY_LIMIT", "MEMORY_LIMIT not configured."),
+                Configuration.GetValue<string>("CF_INSTANCE_INDEX", "CF_INSTANCE_INDEX not configured."),
+                Configuration.GetValue<string>("CF_INSTANCE_ADDR", "CF_INSTANCE_ADDR not configured.")
+            ));
 
-           services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
+            //services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
